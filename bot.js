6fc6482716b7b0d9e5d15f6ec1650d9af5ddb3d6
ESDreamBot <47387876+ESDreamBot@users.noreply.github.com>
2019-02-08 09:46:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1376,7 +1376,6 @@ client.on('message', msg => {
 
 
 
-const moment = require('moment');
 client.on('message',async message => {
   var time = moment().format('Do MMMM YYYY , hh:mm');
   var room;
