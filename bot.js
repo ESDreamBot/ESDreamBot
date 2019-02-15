0ee5617723978f4f093eeb832b0d6bf02e5e491e
ESDreamBot <47387876+ESDreamBot@users.noreply.github.com>
2019-02-15 11:13:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1437,30 +1437,15 @@ client.on('messageReactionRemove', (reaction, user) => {
 
 
 
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-        message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 12960000,
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-          .setDescription("تم أرسال الرابط برسالة خاصة")
-           .setAuthor(client.user.username, client.user.avatarURL)
-                 .setAuthor(client.user.username, client.user.avatarURL)
-                .setFooter('طلب بواسطة: ' + message.author.tag)
 
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        
-    .setDescription("** مدة الرابط : ساعتين | عدد استخدامات الرابط : 100 **")
-      message.author.sendEmbed(Embed11)
-    }
-});
+
+
+
+
+
+
+
+
 
 
 
