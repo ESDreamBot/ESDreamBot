f70e6df9bd3d281abf5194efefbd93619d769877
ESDreamBot <47387876+ESDreamBot@users.noreply.github.com>
2019-02-16 16:13:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1433,10 +1433,28 @@ client.on('message',async message => {
 
 
 
-
-
-
-
+client.on('message', message => {
+  if (true) {
+if (message.content === '=invite') {
+      message.author.send('  https://discordapp.com/api/oauth2/authorize?client_id=546269855716737025&permissions=0&scope=bo   |  تفضل ربط البوت     ').catch(e => console.log(e.stack));
+ 
+    }
+   }
+  });
+ 
+ 
+client.on('message', message => {
+     if (message.content === "=invite") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+     
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 
