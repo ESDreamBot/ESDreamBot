aef90598ce16c54495869e01ef1652a0743f8163
ESDreamBot <47387876+ESDreamBot@users.noreply.github.com>
2019-02-16 18:42:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1459,7 +1459,41 @@ client.on('message', message => {
 
 
 
+client.on('guildCreate', guild => {
+   
+  client.channels.get("543793609384787969")
+const embed = new Discord.RichEmbed()
+   .setAuthor(`بوتك دخل سيرفر جديد مبروك ✅`)
+   .setDescription(`**
+Server name: __${guild.name}__
+Server id: __${guild.id}__
+Server owner: __${guild.owner}__
+Member Count: __${guild.memberCount}__
+Servers Counter : __${client.guilds.size}__**`)
+         .setColor("#f3ae10")
+         .addField("New Server!")
+         .setFooter('ESDream Bot' , client.user.avatarURL)
+           client.channels.get("543793609384787969").send({embed}); //Sup
+}
+ 
+);
 
+client.on('guildDelete', guild => {
+  client.channels.get("543793609384787969")
+const embed = new Discord.RichEmbed()
+   .setAuthor(`للاسف بوتك خرج من السيرفر ❎`)
+   .setDescription(`**
+Server name: __${guild.name}__
+Server id: __${guild.id}__
+Server owner: __${guild.owner}__
+Members Count: __${guild.memberCount}__
+Servers Counter : __${client.guilds.size}__**`)
+         .setColor("#f3ae10")
+         .setFooter('ESDream Bot' , client.user.avatarURL)
+           client.channels.get("543793609384787969").send({embed});
+}
+ 
+);
 
 
 
