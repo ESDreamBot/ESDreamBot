43753962c8692b8dcbde3292ffe257531d03c8e7
ESDreamBot <47387876+ESDreamBot@users.noreply.github.com>
2019-02-19 20:51:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1688,7 +1688,6 @@ client.on('message', message => {
 });
 
 
-
 client.on("message", message => {
  
   if (message.author.bot) return;
@@ -1702,7 +1701,33 @@ client.on("message", message => {
     points: 0,
     credits: 150,
   };
-
+ 
+ 
+fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+});
+ 
+client.on('message', message => {
+ 
+    if(message.content.startsWith(prefix + 'rep')) {
+      if(!message.channel.guild) return;
+                    moment.locale('en');
+                  var getvalueof = message.mentions.users.first()
+                    if(!getvalueof) return message.channel.send(`**:mag: |  ${message.author.username}, the user could not be found.    **`);
+                       if(getvalueof.id == message.author.id) return message.channel.send(`**${message.author.username}, you cant give yourself a reputation !**`)
+    if(profile[message.author.id].reps != moment().format('L')) {
+            profile[message.author.id].reps = moment().format('L');
+            profile[getvalueof.id].rep = Math.floor(profile[getvalueof.id].rep+1);
+         message.channel.send(`** :up:  |  ${message.author.username} has given ${getvalueof} a reputation point!**`)
+        } else {
+         message.channel.send(`**:stopwatch: |  ${message.author.username}, you can raward more reputation  ${moment().endOf('day').fromNow()} **`)
+        }
+       }
+       fs.writeFile('profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+});
  
 client.on("message", (message) => {
   let men = message.mentions.users.first()
@@ -1785,11 +1810,6 @@ message.channel.send(`**:moneybag: | ${message.author.username}, has transferrer
 if (err) console.error(err);
 })
       })//Êã ÇÎÐ ßæÏ ãä ÞÈá Dramex ÕÇÍÈ Probot ÚÔÇä íÔÊÛá ßæÏ áÇÒã ÊÍØ ãáÝ profile.json
-
-
-
-
-
 
 
 
